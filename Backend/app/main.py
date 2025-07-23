9fbe9aec6202cf9c4994d8377c7019fbd544663e
Venuja Prasanjith <144810283+VenujaVP@users.noreply.github.com>
2025-07-23 21:37:42 +0530

    refactor: update comments for server startup instructions

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -1,3 +1,8 @@
+#---------------RUN THIS FILE TO START THE SERVER--------------
+#uvicorn app.main:app --host 192.168.43.73 --port 8005 --reload
+
+#pip install python-dotenv sqlalchemy pymysql bcrypt pydantic[email] passlib pydantic colorlog
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
